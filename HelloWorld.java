61c3e9562dcbb7f37a5f9774bdd234998dab2a45
Joe Quintanilla <joe.quintanilla@my.utsa.edu>
2022-08-14 21:14:31 -0500

    Added a println statement in main

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World, this is my first GitHub project!");
         System.out.println("What does a push do?");
+        System.out.println("What else should I add?");
     }
 }
